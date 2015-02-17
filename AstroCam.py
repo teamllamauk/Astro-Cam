8477a8ff374b67bf2a62c1f3a51135af272544d5
teamllamauk <teamllamauk@users.noreply.github.com>
2015-02-17 14:28:42 +0000

    Update and rename blob-detection.py to AstroCam.py

---
+++ AstroCam.py
@@ -0,0 +1,63 @@
+# Imports
+
+import atexit
+import cPickle as pickle
+import errno
+import fnmatch
+import io
+import os
+import os.path
+import picamera
+import pygame
+import stat
+import threading
+import time
+import yuv2rgb
+from pygame.locals import *
+from subprocess import call  
+
+# Globals
+
+#sizeMode        =  0      # Image size; default = Large
+sizeData = [ # Camera parameters for different size settings
+ # Full res      Viewfinder  Crop window
+ [(2592, 1944), (320, 240), (0.0   , 0.0   , 1.0   , 1.0   )], # Large
+ [(1920, 1080), (320, 180), (0.1296, 0.2222, 0.7408, 0.5556)], # Med
+ [(1440, 1080), (320, 240), (0.2222, 0.2222, 0.5556, 0.5556)]] # Small
+ 
+ # Init
+ 
+os.putenv('SDL_VIDEODRIVER', 'fbcon')
+os.putenv('SDL_FBDEV'      , '/dev/fb1')
+
+rgb = bytearray(320 * 240 * 3)
+yuv = bytearray(320 * 240 * 3 / 2)
+
+pygame.init()
+pygame.mouse.set_visible(False)
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+ 
+camera            = picamera.PiCamera()
+atexit.register(camera.close)
+camera.resolution = sizeData[0][1]
+camera.crop       = (0.0, 0.0, 1.0, 1.0)
+
+# Select screen section to scan
+
+
+# Main
+
+while(True):
+  stream = io.BytesIO() # Capture into in-memory stream
+  camera.capture(stream, use_video_port=True, format='raw')
+  stream.seek(0)
+  stream.readinto(yuv)  # stream -> YUV buffer
+  stream.close()
+  yuv2rgb.convert(yuv, rgb, sizeData[0][1][0], sizeData[0][1][1])
+  img = pygame.image.frombuffer(rgb[0:(sizeData[0][1][0] * sizeData[0][1][1] * 3)], sizeData[0][1], 'RGB')
+    
+  #if img is None or img.get_height() < 240: # Letterbox, clear background
+  #  screen.fill(255,0,0)
+  #if img:
+  screen.blit(img, ((320 - img.get_width() ) / 2, (240 - img.get_height()) / 2))
+  pygame.display.update()
