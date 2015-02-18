6f5625f552141b46614304e98824c469b8d88228
teamllamauk <teamllamauk@users.noreply.github.com>
2015-02-18 14:37:56 +0000

    Create usbtest.py

---
+++ usbtest.py
@@ -0,0 +1,5 @@
+import usb
+for dev in usb.core.find(find_all=True):
+    print "Device:", dev.filename
+    print "  idVendor: %d (%s)" % (dev.idVendor, hex(dev.idVendor))
+    print "  idProduct: %d (%s)" % (dev.idProduct, hex(dev.idProduct))
